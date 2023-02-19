85abcbfcdace7174e9340ddb7942141bc1ddfa8a
Saulo Henrique Aguiar <saulohjr@hotmail.com>
2023-02-19 10:58:12 -0300

    qargtfs commit

--- run.py
+++ run.py
@@ -13,16 +13,16 @@ def main():
 
 
     
-    os.chdir("C:\\Users\\saulo\\FARMSYSTEMIOT\\frontend")
+    os.chdir("C:\\Users\\saulo\\FARMSYSTEMIOTDL\\frontend")
     subprocess.Popen(["python", "dashboard.py"])
     
-    os.chdir("C:\\Users\\saulo\\FARMSYSTEMIOT\\backend\\databaseedge")
+    os.chdir("C:\\Users\\saulo\\FARMSYSTEMIOTDL\\backend\\databaseedge")
     subprocess.Popen(["python", "data.py"])
     
-    os.chdir("C:\\Users\\saulo\\FARMSYSTEMIOT\\backend\\forecastdeeplearningLSTM")
+    os.chdir("C:\\Users\\saulo\\FARMSYSTEMIOTDL\\backend\\forecastdeeplearningLSTM")
     subprocess.Popen(["python", "previsao.py"])
    
-    os.chdir("C:\\Users\\saulo\\FARMSYSTEMIOT\\backend\\databasecloud")
+    os.chdir("C:\\Users\\saulo\\FARMSYSTEMIOTDL\\backend\\databasecloud")
     subprocess.Popen(["python", "datacloud.py"])
     
 
